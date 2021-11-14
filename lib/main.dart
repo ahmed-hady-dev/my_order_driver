c9d4ce0b39b9e9cea88ec48e79d3ce1b729a261d
Ahmed  Abd ElHady <52330103+ahmed-hady-dev@users.noreply.github.com>
2021-11-14 20:46:26 +0200

    #3 home and order view finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_order_driver/view/splash/splash_view.dart';
@@ -13,6 +14,7 @@ late Widget initialScreen;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   //===============================================================
   Bloc.observer = MyBlocObserver();
   DioHelper.init();
